669914148f4b1728af2ac41e321265efb3b3f6ca
sun757 <37966636+sun757@users.noreply.github.com>
2020-10-15 14:54:56 -0400

    preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,37 +4,90 @@ import pandas as pd
 import numpy
 import tempfile
 import numpy as np
-
+import string
 
 def preprocess_calendar():
-    listing_data = df_calendar['listing_id'].astype(dtype='int32')
-    listing_data = listing_data.astype(dtype='int64')
-    if not listing_data.equals(df_calendar['listing_id']):
-        print("ERROR: casting failed. ")
-    else:
-        df_calendar['listing_id'] = df_calendar['listing_id'].astype(dtype='int32')
-
-    df_calendar['available'].replace(to_replace='t', value=True, inplace=True)
-    df_calendar['available'].replace(to_replace='f', value=False, inplace=True)
-    df_calendar['available'] = df_calendar['available'].astype(dtype='bool')
+    df_calendar['listing_id'] = df_calendar['listing_id'].astype(dtype='Int32')
 
     df_calendar.loc[:, 'price'] = df_calendar.loc[:, 'price'].str.strip('$')
     df_calendar['price'].replace(to_replace=r',', value="", inplace=True, regex=True)
     df_calendar['price'] = pd.to_numeric(df_calendar['price'], 'raise', 'signed')
-    df_calendar['price'] = df_calendar['price'].fillna(value=-1)
-    price_data = df_calendar['price'].astype('int32')
-    price_data = price_data.astype('float64')
+    df_calendar['price'] = df_calendar['price'].astype(dtype='Int32')
 
-    if not price_data.equals(df_calendar['price']):
-        print("ERROR: casting failed. ")
-    else:
-        df_calendar['price'] = df_calendar['price'].astype(dtype='int32')
-
-    pd.to_datetime(df_calendar.loc[:, 'date'], infer_datetime_format=True)
     # df_calendar.info()
 
+
 def preprocess_listings():
-    df_listings.info()
+    for j in df_listings.columns:
+        df_listings[j]=df_listings[j].replace(to_replace={r'[^ -~]'},value='',regex=True)
+
+    df_listings.drop(1939, 0,inplace=True)
+
+    # delete cols
+    df_listings.drop('scrape_id', 1,inplace=True)
+    df_listings.drop('last_scraped', 1,inplace=True)
+    df_listings.drop('experiences_offered', 1,inplace=True)
+    df_listings['host_response_time'].replace(to_replace='N/A', value=pd.NA, inplace=True)
+    df_listings['host_response_rate'].replace(to_replace='N/A', value=pd.NA, inplace=True)
+    df_listings.drop('host_acceptance_rate', 1,inplace=True)
+    df_listings.drop('city', 1,inplace=True)
+    df_listings.drop('state', 1,inplace=True)
+
+    df_listings.loc[859, 'zipcode']=98122
+    df_listings['zipcode'] = df_listings['zipcode'].astype("Float32").astype("Int32")
+    df_listings.drop('market', 1,inplace=True)
+    df_listings.drop('smart_location', 1,inplace=True)
+    df_listings.drop('country_code', 1,inplace=True)
+    df_listings.drop('country', 1,inplace=True)
+
+    df_listings.loc[:, 'price'] = df_listings.loc[:, 'price'].str.strip('$')
+    df_listings['price'].replace(to_replace=r',', value="", inplace=True, regex=True)
+    df_listings['price'] = pd.to_numeric(df_listings['price'], 'raise', 'signed')
+    df_listings['price'] = df_listings['price'].astype(dtype='Int32')
+
+    df_listings.loc[:, 'weekly_price'] = df_listings.loc[:, 'weekly_price'].str.strip('$')
+    df_listings['weekly_price'].replace(to_replace=r',', value="", inplace=True, regex=True)
+    df_listings['weekly_price'] = pd.to_numeric(df_listings['weekly_price'], 'raise', 'signed')
+    df_listings['weekly_price'] = df_listings['weekly_price'].astype(dtype='Int32')
+
+    df_listings.loc[:, 'monthly_price'] = df_listings.loc[:, 'monthly_price'].str.strip('$')
+    df_listings['monthly_price'].replace(to_replace=r',', value="", inplace=True, regex=True)
+    df_listings['monthly_price'] = pd.to_numeric(df_listings['monthly_price'], 'raise', 'signed')
+    df_listings['monthly_price'] = df_listings['monthly_price'].astype(dtype='Int32')
+
+    df_listings.loc[:, 'security_deposit'] = df_listings.loc[:, 'security_deposit'].str.strip('$')
+    df_listings['security_deposit'].replace(to_replace=r',', value="", inplace=True, regex=True)
+    df_listings['security_deposit'] = pd.to_numeric(df_listings['security_deposit'], 'raise', 'signed')
+    df_listings['security_deposit'] = df_listings['security_deposit'].astype(dtype='Int32')
+
+    df_listings.loc[:, 'cleaning_fee'] = df_listings.loc[:, 'cleaning_fee'].str.strip('$')
+    df_listings['cleaning_fee'].replace(to_replace=r',', value="", inplace=True, regex=True)
+    df_listings['cleaning_fee'] = pd.to_numeric(df_listings['cleaning_fee'], 'raise', 'signed')
+    df_listings['cleaning_fee'] = df_listings['cleaning_fee'].astype(dtype='Int32')
+
+    df_listings['guests_included'] = pd.to_numeric(df_listings['guests_included'], 'raise', 'signed')
+    df_listings['guests_included'] = df_listings['guests_included'].astype(dtype='int32')
+
+    df_listings.loc[:, 'extra_people'] = df_listings.loc[:, 'extra_people'].str.strip('$')
+    df_listings['extra_people'].replace(to_replace=r',', value="", inplace=True, regex=True)
+    df_listings['extra_people'] = pd.to_numeric(df_listings['extra_people'], 'raise', 'signed')
+    df_listings['extra_people'] = df_listings['extra_people'].astype(dtype='Int32')
+
+    df_listings['minimum_nights'] = pd.to_numeric(df_listings['minimum_nights'], 'raise', 'signed')
+    df_listings['minimum_nights'] = df_listings['minimum_nights'].astype(dtype='int32')
+
+    df_listings['maximum_nights'] = pd.to_numeric(df_listings['maximum_nights'], 'raise', 'signed')
+    df_listings['maximum_nights'] = df_listings['maximum_nights'].astype(dtype='int32')
+    df_listings.drop('calendar_last_scraped', 1,inplace=True)
+
+    df_listings.drop('license', 1,inplace=True)
+    df_listings.drop('jurisdiction_names', 1,inplace=True)
+
+    # df_listings.info()
+
+def preprocess_reviews():
+    for j in df_reviews.columns:
+        df_reviews[j]=df_reviews[j].replace(to_replace={r'[^ -~]'},value='',regex=True)
 
 
 
@@ -47,4 +100,13 @@ with open("data/reviews.csv") as f:
     df_reviews = pd.read_csv("data/reviews.csv")
 
 preprocess_listings()
-#preprocess_calendar()
+preprocess_calendar()
+preprocess_reviews()
+
+with open("data/calendar_pred.csv", "w") as f:
+    df_calendar.to_csv(f, index=False,line_terminator='\n')
+with open("data/listings_pred.csv", "w") as f:
+    df_listings.to_csv(f, index=False, line_terminator='\n')
+with open("data/reviews_pred.csv", "w") as f:
+    df_reviews.to_csv(f, index=False, line_terminator='\n')
+# print(sys.stdout.encoding)
